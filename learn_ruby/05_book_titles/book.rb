71bc8eb1b0c8f60775782dea9b6a77deba3ee3ff
Dustin Seright <dseright@gmail.com>
2016-11-14 03:54:46 -0600

    Solved 05 using algorithm from simon says

--- learn_ruby/05_book_titles/book.rb
+++ learn_ruby/05_book_titles/book.rb
@@ -1,3 +1,17 @@
 class Book
-# write your code here
+
+	attr_accessor :title
+
+	def title
+		phraseArr = @title.split(' ')
+		phraseArr[0].capitalize!
+		littleWords = ['the', 'or', 'and', 'over', 'of', 'in', 'a', 'an']
+		phraseArr.each do |word| 
+			if littleWords.include?(word) == false
+				word.capitalize!
+			end
+		end
+		@title = phraseArr.join(' ')
+	end
+
 end
