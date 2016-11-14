41e9298e02151a54ef2f79d0bac9285db704e8de
Dustin Seright <dseright@gmail.com>
2016-11-14 04:29:42 -0600

    Solved 05 timer

--- learn_ruby/06_timer/timer.rb
+++ learn_ruby/06_timer/timer.rb
@@ -1,3 +1,28 @@
 class Timer
-  #write your code here
+  attr_accessor :seconds
+  def initialize
+  	@seconds = 00
+  	@minutes = 00
+  	@hours = 00
+  end
+  def seconds
+  	@seconds
+  end
+  def minutes
+  	@minutes
+  end
+  def hours
+  	@hours
+  end
+  def time_string
+  	if @seconds >= 60
+  		@minutes = @seconds / 60
+  		@seconds = @seconds % 60
+  	end
+  	if @minutes >= 60
+  		@hours = @minutes / 60
+  		@minutes = @minutes % 60
+  	end
+  	@time_string = [@hours.to_s.rjust(2,'0'), @minutes.to_s.rjust(2, '0'), @seconds.to_s.rjust(2, '0')].join(':')
+  end
 end
